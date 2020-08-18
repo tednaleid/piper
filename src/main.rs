f646d374344d2e78c0bbd41e1e45df9414f53e65
Ted Naleid <contact@naleid.com>
2020-08-17 22:29:25 -0500

    closing but not working with lots of lines

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use piper::args::Args;
 use reqwest::Client;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
-use tokio::sync::mpsc;
-use tokio::task::{self, JoinHandle};
+use std::io::{self, BufRead, BufReader, Stdin};
 use tokio::io::Error;
-use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::{self, Sender};
+use tokio::task::{self, JoinHandle};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -17,21 +16,54 @@ async fn main() -> io::Result<()> {
 
     let worker_handle = spawn_worker(message_receiver);
 
-    spawn_request_context_worker(input, &mut message_sender).await;
+    // spawn_request_context_worker(input, &mut message_sender).await;
+
     // task::spawn(spawn_request_context_worker(input, &mut message_sender)).await;
 
-    // task::spawn(async move {
+    // tokio::spawn(async move {
     //     let url = "http://localhost:1323/100".to_string();
     //     let v = RequestContext { url };
     //     message_sender.send(v).await;
     // }).await;
+
+    // doesn't work
+    //
+    // let reader: Box<dyn BufRead> = if !input.is_empty() {
+    //     Box::new(BufReader::new(File::open(input).unwrap()))
+    // } else {
+    //     Box::new(BufReader::new(io::stdin()))
+    // };
+
+    // TODO try the tokio io aware version of BufRead to see if that works
+
+    let reader: Box<BufReader<Stdin>> = Box::new(BufReader::new(io::stdin()));
+    spawn2(message_sender, reader).await;
+
+    // // TODO how do we get the message sender channel to close without this hack?
+    // // Could we maybe use the tokio io versions of things?
+    // task::spawn( async move { message_sender; }).await;
 
     let _ = worker_handle.await;
 
     Ok(())
 }
 
-async fn spawn_request_context_worker(input: String, message_sender: &mut Sender<RequestContext>) -> () {
+async fn spawn2(mut message_sender: Sender<RequestContext>, reader: Box<BufReader<Stdin>>) {
+    for url_result in reader.lines() {
+        let request_context = RequestContext {
+            url: url_result.unwrap(),
+        };
+
+        if let Err(_) = message_sender.send(request_context).await {
+            return;
+        }
+    }
+}
+
+async fn spawn_request_context_worker(
+    input: String,
+    message_sender: &mut Sender<RequestContext>,
+) -> () {
     let reader: Box<dyn BufRead> = if !input.is_empty() {
         Box::new(BufReader::new(File::open(input).unwrap()))
     } else {
