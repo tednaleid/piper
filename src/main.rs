2f17552e0e6381ffd454ab36ff601c7361583ce4
Ted Naleid <contact@naleid.com>
2020-08-17 22:00:27 -0500

    trying to get it to close

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use reqwest::Client;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use tokio::sync::mpsc;
-use tokio::task::JoinHandle;
+use tokio::task::{self, JoinHandle};
+use tokio::io::Error;
+use tokio::sync::mpsc::Sender;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> io::Result<()> {
     let args = Args::parse();
 
     let Args { input } = args;
@@ -15,6 +17,21 @@ async fn main() {
 
     let worker_handle = spawn_worker(message_receiver);
 
+    spawn_request_context_worker(input, &mut message_sender).await;
+    // task::spawn(spawn_request_context_worker(input, &mut message_sender)).await;
+
+    // task::spawn(async move {
+    //     let url = "http://localhost:1323/100".to_string();
+    //     let v = RequestContext { url };
+    //     message_sender.send(v).await;
+    // }).await;
+
+    let _ = worker_handle.await;
+
+    Ok(())
+}
+
+async fn spawn_request_context_worker(input: String, message_sender: &mut Sender<RequestContext>) -> () {
     let reader: Box<dyn BufRead> = if !input.is_empty() {
         Box::new(BufReader::new(File::open(input).unwrap()))
     } else {
@@ -26,24 +43,19 @@ async fn main() {
             url: url_result.unwrap(),
         };
 
-        // eprintln!("before send url: {}", request_context.url);
         if message_sender.send(request_context).await.is_err() {
             break;
         }
     }
-
-    let _ = worker_handle.await;
 }
 
-fn spawn_worker(mut receiver: mpsc::Receiver<RequestContext>) -> JoinHandle<()> {
+async fn spawn_worker(mut receiver: mpsc::Receiver<RequestContext>) -> () {
     let client = Client::new();
-    tokio::spawn(async move {
-        while let Some(request_context) = receiver.recv().await {
-            let url = request_context.url;
-            let response = client.get(&url).send().await.unwrap();
-            println!("result {} - {}", response.url(), response.status())
-        }
-    })
+    while let Some(request_context) = receiver.recv().await {
+        let url = request_context.url;
+        let response = client.get(&url).send().await.unwrap();
+        println!("result {} - {}", response.url(), response.status())
+    }
 }
 
 #[derive(Debug)]
